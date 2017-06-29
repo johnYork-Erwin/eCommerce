762b387d7fba0322b729630eb13a03cbab557b79
John York-Erwin <solidseussian@gmail.com>
2017-06-29 15:11:13 -0700

    thingy

---
+++ eCommerce/productObject.js
@@ -0,0 +1,14 @@
+const products = [
+  {'name':'Asparagus', 'price':8.99, 'rating':4, 'description':'The winter vegetable that snaps!', 'image':'http://i.dailymail.co.uk/i/pix/2017/02/14/11/3D32011F00000578-4222530-image-a-23_1487071847339.jpg'},
+  {'name':'Squash', 'price':3.99, 'rating':2, 'description':'More winter Vegetables because I miss winter...', 'image':'http://www.aspca.org/sites/default/files/butternut-squash_facebook.jpg'},
+  {'name':'Cabbage', 'price':2.99, 'rating':5, 'description':'Another winter vegetable. Eat up!!! Many varieties.', 'image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7FimsoOIND43kzkmPpwC3jfYWU9xGlTJxjI-qu6q3olhxTOwt'},
+  {'name':'Carrots', 'price':1.99, 'rating':5, 'description':'Are you named Peter? Are you a rabbit? Why risk angry farmers by stealing, just order these here!', 'image':'http://images.wisegeek.com/orange-carrots-against-white.jpg'},
+  {'name':'Cauliflower', 'price':4.99, 'rating':3, 'description':'Do you like a lot of white on your plate? Eat this in place of Broccoli!', 'image':'http://cdn-mf1.heartyhosting.com/sites/mensfitness.com/files/styles/gallery_slideshow_image/public/cauliflower_main.gif?itok=QHvmZUk'},
+  {'name':'Peppers', 'price':1.99, 'rating':2, 'description':'They have no spice but they are sweet!', 'image':'https://static.pexels.com/photos/57426/paprika-vegetables-colorful-food-57426.jpeg'},
+  {'name':'Eggplant', 'price':2.99, 'rating':4, 'description':'Is it a baby bird or is it a plant? Who knows! Cook it up, it is tatsy!', 'image':'http://augustafruit.com/wp-content/uploads/2015/09/1000-x-667-Vegetables.jpg'},
+  {'name':'Broccoli', 'price':2.99, 'rating':0, 'description':'Do you like a lot of green on your plate? Eat this in place of Cauliflower!', 'image':'http://cdn-img.health.com/sites/default/files/styles/400x400/public/styles/main/public/broccoli-better-raw-400x400.jpg?itok=kOwtW5xm'},
+  {'name':'Beets', 'price':1.99, 'rating':5, 'description':'Serve a raw beet salad and watch your dinner guests cringe during each bite.', 'image':'http://www.asianhealthsecrets.com/wp-content/uploads/2011/09/beets-462.jpg'},
+  {'name':'Artichoke', 'price':8.99, 'rating':7, 'description':'Confuse your guests with this flower vegetable!', 'image':'http://www.vegetables.co.nz/assets/vegetables/_resampled/FillWyI0MDAiLCIzMDAiXQ/artichokes-globe.png'}
+];
+
+var orderedProducts = [];
---
+++ eCommerce/products/products.js
@@ -0,0 +1,73 @@
+let $list = $('#salesList');
+let criteria = {'price':Infinity, 'rating':0};
+
+//Add to cart button, function that adds to cart is specific to card layout on this page
+const addButtonListeners = function() {
+  $('.btn-floating').on('click', function() {
+    let name = event.target.parentElement.parentElement.innerText.toLowerCase().slice(0, -4);
+    console.log(name);
+    products.forEach(function(product) {
+      if (product['name'].toLowerCase() === name) {
+        console.log(orderedProducts);
+        orderedProducts.push(product);
+        console.log(orderedProducts);
+        return;
+      }
+    });
+  });
+};
+
+renderProducts();
+addButtonListeners();
+
+function renderProducts() {
+  $list.empty();
+  products.forEach(function(product) {
+    //Builds a new row for the product IF it meets the criteria laid out
+    if (product['price'] < criteria['price'] && product['rating'] >= criteria['rating']) {
+      let $button = $('<a>').addClass('btn-floating halfway-fab waves-effect waves-light red');
+      $button.append($('<i>').addClass('material-icons').text('add'));
+
+      let $rightCol = $('<div>').addClass('col l9 right');
+      let $rowRight = $('<div>').addClass('row center-align').append($('<h4>').text(product.name + ' ').append($button));
+
+      $rightCol.append($rowRight.append($('<p>').text(product['description']).addClass('col l11 center center-align')));
+
+      let $rightColBot = $('<div>').addClass('row');
+      $rightColBot.append($('<div>').addClass('col l6 left center-align').append($('<h4>').text('Price: $' + product['price'])));
+      $rightColBot.append($('<div>').addClass('col l6 right center-align').append($('<h4>').text('Rating: ' + product['rating']+'/5')));
+
+      $rightCol.append($rightColBot);
+
+      let $image = $('<img>');
+      $image.attr({
+        src: product['image'],
+        alt: product['name']
+      });
+
+      let $row = $('<div>').addClass('row valign-wrapper').css({
+               "border-weight":"1px",
+               "border-style":"solid"}
+             ).append($image).append($rightCol);
+      $list.append($row);
+    }
+  });
+}
+
+$('#priceCriteria').on('click', function() {
+  if (event.target.tagName === 'A') {
+    let price = event.target.innerText.replace('$','');
+    criteria['price'] = Number(price);
+  }
+  renderProducts();
+  addButtonListeners();
+});
+
+$('#ratingCriteria').on('click', function() {
+  if (event.target.tagName === 'A') {
+    criteria['rating'] = Number(event.target.innerText.charAt(0));
+  }
+  renderProducts();
+  //after any render products we need to re-add event listeners
+  addButtonListeners();
+});
