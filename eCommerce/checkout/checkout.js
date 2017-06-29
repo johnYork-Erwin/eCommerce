e10d9b2ccc0c4626e0e8c7e7761d9471ae6ae8a5
Brian James <brianpja@gmail.com>
2017-06-29 14:46:18 -0700

    brians changes

--- eCommerce/checkout/checkout.js
+++ eCommerce/checkout/checkout.js
@@ -1,74 +1,136 @@
 //materialize initialization for 'select'
 $(document).ready(function() {
-    $('select').material_select();
-  });
+  $('select').material_select();
+});
 
 
+const inputNames = ['firstNameShip', 'lastNameShip', 'companyShip', 'addressShip', 'aptShip', 'cityShip', 'zipShip']
 
-
-
-
-
-const inputNames = ['firstNameShip', 'lastNameShip', 'companyShip', 'addressShip', 'aptShip', 'cityShip', 'stateShip', 'zipShip']
-
-
-//form validation
 
 
 //this copies the shipping info into billing info
-    const checkbox = $('#filled-in-box');
+const checkbox = $('#filled-in-box');
 
-    checkbox.on('click', (event) => {
-      console.log('i clicked', event.target.checked);
+checkbox.on('click', (event) => {
 
-      if (event.target.checked) {
+  const state = $('select[name="stateShip"]');
+  const stateValue = state.val();
 
-        for (const inputName of inputNames) {
+//cycles through list of input field names
+  for (const inputName of inputNames) {
 
-          const value = $(`input[name=${inputName}]`).val();
-          console.log(inputName, value);
+    const value = $(`input[name=${inputName}]`).val();
 
-          const newName = inputName.replace('Ship', 'Bill');
-          console.log(newName)
-          $(`input[name=${newName}]`).val(value);
-        }
-      }
-    })
+    const newName = inputName.replace('Ship', 'Bill');
 
 
-    $('form').on('submit', (event) => {
-      event.preventDefault();
-      console.log('submitting!')
+    if (event.target.checked) {
+      //copies shipping info to billing
+      $(`input[name=${newName}]`).val(value);
+      $('select[name="stateBill"]').val(stateValue);
+    } else {
+      //empty billing information
+      $(`input[name=${newName}]`).val('');
+      $('select[name="stateBill"]').val('');
+    }
+  }
 
-      Materialize.toast('Thank you for submitting!', 4000);
+});
 
-      const firstNameShip = $('input[name="firstNameShip"]').val();
-      console.log(firstNameShip);
-      const lastNameShip = $('input[name="lastNameShip"]').val();
-      const companyShip = $('input[name="companyShip"]').val();
-      const addressShip = $('input[name="addressShip"]').val();
-      const aptShip = $('input[name="aptShip"]').val();
-      const cityShip = $('input[name="cityShip"]').val();
-      const stateShip = $('input[name="stateShip"]').val();
-      const zipShip = $('input[name="zipShip"]').val();
 
-      const firstNameBill = $('input[name="firstNameBill"]').val();
-      const lastNameBill = $('input[name="lastNameBill"]').val();
-      const companyBill = $('input[name="companyBill"]').val();
-      const addressBill = $('input[name="addressBill"]').val();
-      const aptBill = $('input[name="aptBill"]').val();
-      const cityBill = $('input[name="cityBill"]').val();
-      const stateBill = $('input[name="stateBill"]').val();
-      const zipBill = $('input[name="zipBill"]').val();
+//form validation
+$('form').on('submit', (event) => {
+  event.preventDefault();
 
-      const validationArray = $('input[class="validate"]').val();
-      console.log(validationArray);
-      for (const inputField of validationArray) {
-        if (inputField.val() === '') {
-          console.log(inputField[name]);
-        }
-      }
-      // if (email !== emailConfirm) {
-      //     alert('ohhhhh you better match those emails');
-      // }
-    });
+  let validity = true;
+  let zipValid = true;
+  let ccValid = true;
+
+//basic validation checking for presence
+  const inputsThatNeedPresence = $('.validate');
+  console.log(inputsThatNeedPresence);
+  console.log(inputsThatNeedPresence[0]);
+
+  for (let i = 0; i < inputsThatNeedPresence.length; i++) {
+    console.log(inputsThatNeedPresence.eq(i).val());
+    if (inputsThatNeedPresence.eq(i).val().length > 0) {
+      inputsThatNeedPresence[i].style.background = 'white';
+    } else {
+      inputsThatNeedPresence[i].style.background = '#ffdddd';
+      validity = false;
+    }
+  }
+
+//validates zip codes
+  const zipCodes = $('.zip');
+
+  for (let i = 0; i < zipCodes.length; i++) {
+    const value = zipCodes.eq(i).val();
+    console.log(isNaN(parseFloat(value)));
+    if (isNaN(parseFloat(value)) === false && 5 <= value.length && value.length <= 9) {
+      zipCodes[i].style.background = 'white';
+    } else {
+      zipCodes[i].style.background = '#ffdddd';
+      validity = false;
+      zipValid = false;
+    }
+  }
+
+//validates cc number
+  const ccNumber = $('input[name="cc"]');
+  console.log(ccNumber);
+  if (isNaN(parseFloat(ccNumber.val())) === false && ccNumber.val().length >= 16) {
+    ccNumber[0].style.background = 'white';
+  } else {
+    ccNumber[0].style.background = '#ffdddd';
+    validity = false;
+    ccValid = false;
+  }
+
+//validates dropdown selections
+  const selectInputs = $('select');
+  console.log(selectInputs);
+  for (let i = 0; i < selectInputs.length; i++) {
+    const value = selectInputs.eq(i).val();
+    console.log(value);
+    if (value) {
+      selectInputs[i].style.background = 'white';
+    } else {
+      selectInputs[i].style.background = '#ffdddd';
+      validity = false;
+    }
+  }
+
+//creates toasts
+  if (validity) {
+    Materialize.toast('Thank you for your order!', 4000);
+  } else {
+    Materialize.toast('Not valid! Please enter required fields', 4000);
+  }
+
+  if (zipValid === false) {
+    Materialize.toast('Enter a proper zip code!', 4000);
+  }
+
+  if (ccValid === false) {
+    Materialize.toast('Enter a proper credit card number!', 4000);
+  }
+});
+
+
+//dynamically adds states
+const states = ['CA', 'ID', 'MT', 'OR', 'WA'];
+const selectStateShip = $('select[name="stateShip"]');
+console.log(selectStateShip);
+const selectStateBill = $('select[name="stateBill"]');
+//create an option tag, add state from array as text,
+//add a value that increases each time, then append each option tag to both select fields
+let optionValue = 0;
+states.forEach((stateName) => {
+  optionValue++;
+  const optionShip = $('<option>');
+  optionShip.text(stateName).attr('value', optionValue);
+  const optionBill = optionShip.clone()
+  selectStateShip.append(optionShip);
+  selectStateBill.append(optionBill);
+});
